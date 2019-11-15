330f4335eacc83a1817b3ffaeb091ba4c29d8709
the-monio <cauaaquino11@gmail.com>
2019-11-15 12:02:23 -0200

    Add tela de Play, e filme 1

---
+++ src/moviequiz/Moviequiz.java
@@ -0,0 +1,14 @@
+package moviequiz;
+
+import Telas.Teladeplay;
+
+/**
+ *
+ * @author David & Emanuel
+ */
+public class Moviequiz {
+    public static void main(String[] args) {
+        new Teladeplay().setVisible(true);
+    }
+    
+}
